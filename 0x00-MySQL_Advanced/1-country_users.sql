3614390fd656fa3e2cd1a8d71ace9c5d475bc018
Jamescog <jamescog72@gmail.com>
2023-02-11 06:38:39 +0000

    task 2 completed

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- Create the "users" table if it doesn't already exist
+-- with attributes: id (integer, auto increment, primary key, never null), 
+-- email (string, unique, never null), name (string), 
+-- country (enumeration of US, CO, TN, default US, never null)
+CREATE TABLE IF NOT EXISTS users(
+	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NUL DEFAULT  'US'
+	);
