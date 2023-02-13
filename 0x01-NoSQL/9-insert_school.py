982286de0f0dfbdea9c6f1c11176ab68f5137f6c
Jamescog <jamescog72@gmail.com>
2023-02-13 20:59:47 +0300

    syntax fix

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -7,4 +7,4 @@ file: 9-insert_school.py
 def insert_school(mongo_collection, **kwargs):
     """ Python function that inserts a new document in a collection based on kwargs
     """
-    return mongo_collection.insert_one(**kwargs).inserted_id
+    return mongo_collection.insert_one(kwargs).inserted_id
