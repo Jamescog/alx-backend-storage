452ae0ab127bd359b80bf7ee9936468566740794
Jamescog <jamescog72@gmail.com>
2023-02-13 22:18:33 +0300

    update mongodb through pymongo

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+file: 10-update_topics.py
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Python function that changes all topics of a school document based on the name
+    """
+
+    mongo_collection.update({
+        name:"name"
+    },
+    {
+        "$set": {
+            topics: topics
+        }
+    }
+    )
+    return mongo_collection
