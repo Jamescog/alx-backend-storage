be785d007ec6564a94c5fcf15103e7dafed41c2c
Jamescog <jamescog72@gmail.com>
2023-02-13 22:23:56 +0300

    update_one

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -8,13 +8,10 @@ def update_topics(mongo_collection, name, topics):
     """Python function that changes all topics of a school document based on the name
     """
 
-    mongo_collection.update({
+    mongo_collection.update_one({
         name:"name"
     },
-    {
-        "$set": {
-            topics: topics
-        }
-    }
+    
+        {"$set": {"topic": topics}}
     )
     return mongo_collection
